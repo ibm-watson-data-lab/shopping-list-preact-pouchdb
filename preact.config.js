be9cc64ca7a02784a12d3d50c89b0fafd128ba47
Raj Singh <rrsingh@us.ibm.com>
2017-09-21 15:59:07 -0400

    output to docs instead of build

--- preact.config.js
+++ preact.config.js
@@ -1,4 +1,9 @@
+const path = require('path');
+
 export default config => {
   config.resolve.mainFields = ["browser", "module", "main"];
   config.node.process = 'mock';
+  config.target = 'web';
+  config.output.path = path.resolve(__dirname, 'docs');
+  config.output.publicPath = './';
 };
